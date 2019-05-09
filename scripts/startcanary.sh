73c3bf8a1afecd9302a9cbccf243139c00afba3c
Dario B <feddar@users.noreply.github.com>
2019-05-09 21:56:05 +0200

    Update startcanary.sh

--- scripts/startcanary.sh
+++ scripts/startcanary.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #exec tail -f /data/opencanary.log &
 
-HOSTNAME=${HOSTNAME:-$(hostname -f)}
+OPENCANARY_NODE_ID=${OPENCANARY_NODE_ID:-$(hostname -f)}
 
-sed -i "s/device\.node_id.*/device\.node_id\": \"$HOSTNAME\",/" /root/.opencanary.conf
+sed -i "s/device\.node_id.*/device\.node_id\": \"$OPENCANARY_NODE_ID\",/" /root/.opencanary.conf
 
 source /opt/opencanary/virtualenv/bin/activate && opencanaryd --dev
