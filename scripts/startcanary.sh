f0d3404ebfb468d82144d93b44db673ab99767db
Dario B <feddar@users.noreply.github.com>
2020-04-02 14:51:15 +0200

    change opencanary startup
    Modified opencanary startup, because --dev logs lot of messages. With --start opencanary goes in background, that's why the while true loop

--- scripts/startcanary.sh
+++ scripts/startcanary.sh
@@ -5,4 +5,6 @@ OPENCANARY_NODE_ID=${OPENCANARY_NODE_ID:-$(hostname -f)}
 
 sed -i "s/device\.node_id.*/device\.node_id\": \"$OPENCANARY_NODE_ID\",/" /root/.opencanary.conf
 
-source /opt/opencanary/virtualenv/bin/activate && opencanaryd --dev
+source /opt/opencanary/virtualenv/bin/activate && opencanaryd --start
+
+exec bash -c 'while true; do sleep 5; done'
